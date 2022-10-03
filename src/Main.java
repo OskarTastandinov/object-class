dde65e51cbe973fdb56b7f88fe25fa2fa6823a96
askar <tashtandinov.oskar@yandex.ru>
2022-10-03 22:27:30 +0700

    первый коммит по объектам и классам

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,16 @@
-import jdk.jshell.PersistentSnippet;
+
 
 public class Main {
-
     public static void main(String[] args){
-        String nameJohn = "John";
-        int ageJohn = 13;
+        Author SE = new Author("Сергей", "Есенин");
+        Author AD = new Author("Александр","Пушкин");
+        Book LukDub = new Book("У лукоморья дуб зелёный",AD,1820);
+        Book pismokzhenshine = new Book("Письмо к женщине",SE,1924);
+        System.out.println("Книга: " + LukDub.getTitle() + " Автор: " + LukDub.getAuthor().getName() + " " + LukDub.getAuthor().getSurname() + " Год публикации " + LukDub.getPublicationYear());
 
-        String nameSarah = "Sarah";
-        int ageSarah = 30;
 
-        String[] names = {"John", "Sarah"};
-        int[] ages = {13,30};
-
-            for (int i = 0; i < names.length; i++) {
-            System.out.println("Имя = " + names[i] + " Возраст = " + ages[i]);
-        }
-        Person sarah = new Person("Sarah", 30);
-        System.out.println("sarah.name = " + sarah.getName());
-        System.out.println("sarah.age = " + sarah.getAge());
-        sarah.setAge(31);
-        System.out.println("sarah.getAge() = " + sarah.getAge());
-        Person John = new Person("John", 13);
     }
+
+
 
 }
